4b42b47a3016370e977d26e6d15c8ad1627af03b
Asfand7867 <161638610+Asfand7867@users.noreply.github.com>
2025-02-11 18:24:55 -0700

    added gradient
    front page css

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { router } from 'expo-router';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Welcome() {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={['#1B2845', '#537895']}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
       <Text style={styles.title}>Welcome to My New App</Text>
-      <TouchableOpacity 
-        style={styles.button} 
-        onPress={() => router.push('/login/')}
+      <TouchableOpacity  
+      onPress={() => router.push('/login/')}
+      >
+      <LinearGradient
+        colors={['#202020', 'black']}
+        style={styles.button}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
       >
         <Text style={styles.buttonText}>Go to Login</Text>
+      </LinearGradient>
       </TouchableOpacity>
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -24,13 +36,21 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     marginBottom: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
   },
   button: {
-    backgroundColor: '#007bff',
+    backgroundColor: 'black',
     padding: 15,
     borderRadius: 5,
+    shadowColor: 'rgba(0, 0, 0, 0.75)',
+    shadowOffset: { width: 2, height: 2 },
+    shadowRadius: 5,
   },
   buttonText: {
     color: 'white',

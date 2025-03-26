3f8784bba4518b43cefa50f115891d55ac3bbed7
CraftyNuker <gallasicnoah@gmail.com>
2025-03-26 02:37:54 -0600

    Updated stuff
    Changed it so that you'll need a .env file to make this work.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,11 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase URL or Anon Key");
-}
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,

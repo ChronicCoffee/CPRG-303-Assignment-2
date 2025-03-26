f618211e33fef3620957d4ff34e37c14568aef06
CraftyNuker <gallasicnoah@gmail.com>
2025-03-26 01:17:50 -0600

    Update supabase.ts
    Changed it so that you'll have to create a .env file for the keys from Supabase.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,13 @@
-
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://caailgyvpzdiujoojrqk.supabase.co'; // Supabase URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhYWlsZ3l2cHpkaXVqb29qcnFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2MTU3MTksImV4cCI6MjA1ODE5MTcxOX0.4pejlGLchwyW-VXe4oBXml5Re0Q8AQ1IHYjKdlYPBQY'; // Supabase API Key
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase URL or Anon Key');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
